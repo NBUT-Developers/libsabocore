7c4c0352b64c02c04dc33c59aa7fc8a69c418fce
Alex <chao.zhang@upai.com>
2017-01-25 15:03:31 +0800

    libsabo_core fist commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+#################################################
+# Copyright 2016-2017 Alex(zchao1995@gmail.com) #
+#################################################
+
+CC=gcc
+CFLAGS=-O2 -Wall
+DY=-shared -fPIC
+AR=ar
+
+
+default: so a
+
+so:
+	$(CC) sabo_core.c $(CFLAGS) $(DY) -o libsabocore.so
+
+a:
+	$(CC) sabo_core.c -c $(CFLAGS)
+	$(AR) crv libsabocore.a sabo_core.o
+
+clean:
+	rm -f *.so *.a *.o
---
+++ sabo_core.h
@@ -0,0 +1,58 @@
+
+/*
+ * Copyright (C) 2016-2017  Alex(zchao1995@gmail.com)
+ */
+
+#ifndef SABO_CORE_H
+#define SABO_CORE_H
+
+#define   SABO_AC        0
+#define   SABO_PE        1
+#define   SABO_TLE       2
+#define   SABO_MLE       3
+#define   SABO_WA        4
+#define   SABO_RE        5
+#define   SABO_RE_DBZ    6
+#define   SABO_RE_SO     7
+#define   SABO_SYSERR    8
+#define   SABO_OLE       9
+#define   SABO_MC        10
+
+typedef struct {
+
+    char       *code_bin_file;
+    char       *executor;
+    
+    char       *spj_code_bin;
+    char       *spj_executor;
+    int         spj_mode;
+
+    int         data_in_fd;
+    int         data_out_fd;
+    int         user_out_fd;
+
+    char      **allow_so_file;
+    int         allow_so_file_n;
+
+    int        *allow_sys_call;
+    int         allow_sys_call_n;
+
+    int         time_limits;
+    int         memory_limits;
+
+    int         use_sandbox; /* FIXME, just for java now */
+
+    const char *classpath; /* FIXME, for java class path */
+
+} sabo_ctx_t;
+
+typedef struct {
+    int time_used;
+    int memory_used;
+    int judge_flag;
+
+} sabo_res_t;
+
+
+const char *sabo_core_run(sabo_ctx_t *ctx, sabo_res_t *info);
+#endif
