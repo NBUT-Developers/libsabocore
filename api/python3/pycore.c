27a67f8e2a4156a0d51377af6e6a790396694f33
Alex <chao.zhang@upai.com>
2017-01-31 18:57:04 +0800

    add python3 api

--- api/python3/pycore.c
+++ api/python3/pycore.c
@@ -1,10 +1,6 @@
-#include <sabo_core.h>
+#include "sabo_core.h"
 #include <Python.h>
-
-
-PyMODINIT_FUNC PyInit_sabo_core(void);
-static PyObject* py_run(PyObject *self, PyObject *args);
-char* process_arg(PyObject *arg);
+#include <stdlib.h>
 
 
 char*
@@ -17,47 +13,23 @@ process_arg(PyObject *arg) {
 static PyObject*
 py_run(PyObject *self, PyObject *args) {
     PyObject* judge_config;
-    sabo_run_config config;
+    sabo_ctx_t ctx;
     PyArg_ParseTuple(args, "O", &judge_config);
 
-    config.exe           = process_arg(PyDict_GetItemString(judge_config, "exe"));
-    config.code_path     = process_arg(PyDict_GetItemString(judge_config, "code_path"));
-    config.in_path       = process_arg(PyDict_GetItemString(judge_config, "in_path"));
-    config.out_path      = process_arg(PyDict_GetItemString(judge_config, "out_path"));
-    config.user_path     = process_arg(PyDict_GetItemString(judge_config, "user_path"));
-    config.spj_path      = process_arg(PyDict_GetItemString(judge_config, "spj_path"));
-    config.time_limits   = atoi(process_arg(PyDict_GetItemString(judge_config, "time_limits")));
-    config.memory_limits = atoi(process_arg(PyDict_GetItemString(judge_config, "memory_limits")));
-    config.is_spj        = atoi(process_arg(PyDict_GetItemString(judge_config, "is_spj")));
-    config.use_sandbox   = atoi(process_arg(PyDict_GetItemString(judge_config, "use_sandbox")));
-    config.err_path      = "/dev/null";
+    ctx.executor      = process_arg(PyDict_GetItemString(judge_config, "executor"));
+    ctx.data_in_fd    = atoi(process_arg(PyDict_GetItemString(judge_config, "data_in_fd")));
+    ctx.user_out_fd   = atoi(process_arg(PyDict_GetItemString(judge_config, "user_out_fd")));
+    ctx.time_limits   = atoi(process_arg(PyDict_GetItemString(judge_config, "time_limits")));
+    ctx.memory_limits = atoi(process_arg(PyDict_GetItemString(judge_config, "memory_limits")));
+    ctx.use_sandbox   = atoi(process_arg(PyDict_GetItemString(judge_config, "use_sandbox")));
 
-    sabo_result_info res;
-    res.judge_flag = SABO_UNKNOWN;
-
-    if (config.code_path == NULL || config.in_path == NULL ||
-        config.out_path == NULL || config.user_path == NULL ||
-        (config.is_spj && config.spj_path == NULL) ||
-         (config.use_sandbox == 0 && config.exe == NULL)
-         || config.time_limits == 0 || config.memory_limits == 0) {
-        res.judge_flag = SABO_SYSERR;
-
-        return Py_BuildValue("(iii)", res.judge_flag, 0, 0);
-    }
-
+    sabo_res_t res;
 
     res.time_used = -1;
     res.memory_used = -1;
-    sabo_core_init();
-    sabo_core_run(&config, &res);
+    const char *err = sabo_core_run(&ctx, &res);
 
-    if (res.judge_flag == SABO_TLE) {
-        res.time_used = config.time_limits;
-    } else if (res.judge_flag == SABO_MLE) {
-        res.memory_used = config.memory_limits;
-    }
-
-    return Py_BuildValue("(iii)", res.judge_flag, res.time_used, res.memory_used);
+    return Py_BuildValue("(siii)", err, res.judge_flag, res.time_used, res.memory_used);
 }
 
 
--- api/python3/setup.py
+++ api/python3/setup.py
@@ -17,9 +17,9 @@
 from distutils.core import setup, Extension
 
 setup(name='sabo_core',
-    version='1.0',
+    version='1.1',
     description='sabo judge core',
     ext_modules = [
-        Extension('sabo_core', ['sabo_core.c'])
+        Extension('sabo_core', ['sabo_core.c', 'pycore.c'])
     ]
 )
