3c774c7e5807a2d45c5b5bd2e88defa3dfeeaa71
Alex <chao.zhang@upai.com>
2017-01-31 15:47:32 +0800

    add java cases

---
+++ t/test_java_rundone/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner (System.in);
+        int a, b;
+        while (scan.hasNextInt())
+        {
+            a = scan.nextInt();
+            b = scan.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
